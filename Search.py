debf2b00a9c5283ff98914dfdfd3ac21c657f776
FynnMc <39341232+FynnMc@users.noreply.github.com>
2019-05-07 15:43:23 +0100

    Linear and Binary Searches

--- Search.py
+++ Search.py
@@ -1,1 +1,69 @@
+from random import randint
+import time
 
+# Search Algorithms on Sorted, Uniform data
+
+sorted_l = []
+for i in range(200000000):
+    sorted_l.append(i)
+
+
+# Linear Search
+
+def lin_search(sorted_list, element):
+    for i in range(len(sorted_list)):
+        if sorted_list[i] == element:
+            return print(i)
+
+
+# Binary Search - Recursive
+
+# Creating a sub-array each time will result in bad performance for large arrays.
+# It's better to pass in the bounds of the array instead.
+
+
+def bin_search_r(sorted_list, l, r, element):
+    if r >= l:
+        mid = l + (r - l)//2
+        if sorted_list[mid] == element:
+            return print(mid)
+        elif sorted_list[mid] > element:
+            return bin_search_r(sorted_list, l, mid - 1, element)
+        else:
+            return bin_search_r(sorted_list, mid + 1, r, element)
+    else:
+        return print('Error: Empty List')
+
+# Binary Search - Iterative
+
+
+def bin_search_i(sorted_list, l, r, element):
+
+    while l <= r:
+
+        mid = l + (r-l)//2
+
+        if sorted_list[mid] == element:
+            return print(mid)
+        elif sorted_list[mid] < element:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return print("Element Not Present")
+
+
+start = time.time()
+lin_search(sorted_l, 19879999)
+end = time.time()
+print(end-start)
+
+start = time.time()
+bin_search_r(sorted_l, 0, len(sorted_l)-1, 19879999)
+end = time.time()
+print(end-start)
+
+start = time.time()
+bin_search_i(sorted_l, 0, len(sorted_l)-1, 19879999)
+end = time.time()
+print(end-start)
