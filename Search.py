8b2b66a5f3e8229519ec443a05bdf4ddc0f6203d
FynnMc <39341232+FynnMc@users.noreply.github.com>
2019-05-08 09:16:25 +0100

    Added Interpolation Search

--- Search.py
+++ Search.py
@@ -32,7 +32,7 @@ def bin_search_r(sorted_list, l, r, element):
         else:
             return bin_search_r(sorted_list, mid + 1, r, element)
     else:
-        return print('Error: Empty List')
+        return print('Error: Element Not In List') # After they have become equal l will then exceed r hence exit
 
 # Binary Search - Iterative
 
@@ -52,6 +52,29 @@ def bin_search_i(sorted_list, l, r, element):
 
     return print("Element Not Present")
 
+# Interpolation Search
+
+
+def interp_search(sorted_list, l, r, element):
+    if r > l:
+        ind_select = l + int(round((r - l)*(element - sorted_list[l])/(sorted_list[r] - sorted_list[l])))
+        if sorted_list[ind_select] == element:
+            return print(ind_select)
+        elif sorted_list[ind_select] > element:
+            return bin_search_r(sorted_list, l, ind_select - 1, element)
+        else:
+            return bin_search_r(sorted_list, ind_select + 1, r, element)
+    elif r == l:
+        ind_select = l
+        if sorted_list[ind_select] == element:
+            return print(ind_select)
+        elif sorted_list[ind_select] > element:
+            return bin_search_r(sorted_list, l, ind_select - 1, element)
+        else:
+            return bin_search_r(sorted_list, ind_select + 1, r, element)
+    else:
+        return print('Error: Element Not In List')
+
 
 start = time.time()
 lin_search(sorted_l, 19879999)
@@ -65,5 +88,10 @@ print(end-start)
 
 start = time.time()
 bin_search_i(sorted_l, 0, len(sorted_l)-1, 19879999)
+end = time.time()
+print(end-start)
+
+start = time.time()
+interp_search(sorted_l, 0, len(sorted_l)-1, 19879999)
 end = time.time()
 print(end-start)
